15627c9105e76df51e04ae026879b8a1d51adaa0
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-10-14 17:40:55 +0200

    margin

--- src/components/AnimeMini.js
+++ src/components/AnimeMini.js
@@ -2,11 +2,11 @@ import React from "react"
 const axios = require("axios").default;
 
 var AnimeMini = props => (
-    <div className="anime-mini card col-md-3">
+    <div className="anime-mini card col-md-3 p-2">
         <img class="card-img-top" src={props.animeMini.image_url} alt="Card image cap"></img>
         <div className="card-body">
             <h5 className="class-title">{props.animeMini.title}</h5>
-            <h2></h2>
+            <h2>{props.animeMini.score}</h2>
         </div>
     </div>
 );
