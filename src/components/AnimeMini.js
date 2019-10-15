bc56e3c97a2c50483df64169916095087d6850df
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-10-15 15:31:11 +0200

    changed card size

--- src/components/AnimeMini.js
+++ src/components/AnimeMini.js
@@ -2,7 +2,7 @@ import React from "react"
 const axios = require("axios").default;
 
 var AnimeMini = props => (
-    <div className="anime-mini card col-md-3 p-2">
+    <div className="anime-mini card col-md-2 p-2">
         <img class="card-img-top" src={props.animeMini.image_url} alt="Card image cap"></img>
         <div className="card-body">
             <h5 className="class-title">{props.animeMini.title}</h5>
