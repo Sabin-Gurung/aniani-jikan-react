a7de117d8406c3eaf8a434d3e6596dc8e1178c7c
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-10-15 16:53:43 +0200

    form creation

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ const AnimeMini = require("./components/AnimeMini").default;
 
 
 class App extends React.Component {
-  state={
-    animeResults : []
+  constructor(){
+    super();
+    this.state = {
+      animeResults: [],
+      yearsOptions:[]
+    }
+
+    let currentYear = new Date().getFullYear();
+    for (var i = 1950; i <= currentYear; ++i){
+      this.state.yearsOptions.push(i);
+    }
   }
 
   componentDidMount(){
@@ -25,9 +34,35 @@ class App extends React.Component {
         <div className="header row">
           AniANI-Jikan
         </div>
-        <div className="searchSection row">
+        {/* <div className="searchSection row">
           <div className="col-md-6 offset-md-3">
             <input type="text" className="form-control" placeholder="Enter keyword to serach Anime" />
+          </div>
+        </div> */}
+        <div className="row seasonSearchSection">
+          <div class="input-group col-md-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text">Year</span>
+            </div>
+            <select class="form-control">
+              {
+                this.state.yearsOptions.map(q => <option>{q}</option>)
+              }
+            </select>
+          </div>
+          <div class="input-group col-md-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text">Season</span>
+            </div>
+            <select class="form-control">
+              <option value="summer">summer</option>
+              <option value="winter">winter</option>
+              <option value="fall">fall</option>
+              <option value="spring">spring</option>
+            </select>
+          </div>
+          <div className="col-md-3">
+            <button className="btn btn-success">Go</button>
           </div>
         </div>
         <div className="anime-results row">
