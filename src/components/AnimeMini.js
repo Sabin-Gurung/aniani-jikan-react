9b94beb010bbce574aaabc35ba122c241320efc2
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-10-16 12:27:59 +0200

    card onclick handler

--- src/components/AnimeMini.js
+++ src/components/AnimeMini.js
@@ -1,14 +1,23 @@
 import React from "react"
 import './AnimeMini.css'
 
-var AnimeMini = props => (
-    <div className="anime-mini col-sm-4 col-md-3 col-lg-2 mt-3">
-        <img src={props.animeMini.image_url} alt="Card image cap"></img>
-        <div>
-            <p>{props.animeMini.title}</p>
-            <p>{props.animeMini.score}</p>
-        </div>
-    </div>
-);
+class AnimeMini extends React.Component {
+
+    openPopUpHandler = ()=>{
+        alert(`opening information of ${this.props.animeMini.title}`)
+    }
+
+    render() {
+        return (
+            <div className="anime-mini col-sm-4 col-md-3 col-lg-2 mt-3" onClick={this.openPopUpHandler}>
+                <img src={this.props.animeMini.image_url} alt="Card image cap"></img>
+                <div>
+                    <p>{this.props.animeMini.title}</p>
+                    <p>{this.props.animeMini.score}</p>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default AnimeMini;
