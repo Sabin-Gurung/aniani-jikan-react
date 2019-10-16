5e33f85bfc6abc7f5653050f1620eb48e2ebf7df
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-10-16 15:32:42 +0200

    adding anime detail to main page

--- src/components/AnimeDetail.js
+++ src/components/AnimeDetail.js
@@ -1,18 +1,36 @@
 import React from "react";
 
+const axios = require("axios").default;
+
 class AnimeDetail extends React.Component{
+    state = {anime : null};
 
     componentDidMount(){
-        
+        axios.get(`https://api.jikan.moe/v3/anime/${this.props.animeId}`)
+        .then(res=>res.data)
+        .then(res=>{
+            this.setState({anime : res});
+        });
     }
 
     render(){
+        let anime = this.state.anime;
         return(
             <>
-            <div className="col-md-12">
-                <h1>{this.props.animeId}</h1>
-                <button onClick={this.props.closeBtnHandler}>Close</button>
-            </div>
+                <div className="col-md-12">
+                    {
+                        anime ? 
+                        (
+                            <>
+                            <h1>{anime.title}</h1>
+                            <img src={anime.image_url}></img>
+                            <button onClick={this.props.closeBtnHandler}>Close</button>
+                            </>
+                        ) 
+                        : 
+                        (<h1>Loading</h1>)
+                    }
+                </div>
             </>
         );
     }
