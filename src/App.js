dbb6a9c8380515c35793969dfdc598723b3b7f14
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-10-16 12:05:46 +0200

    moved view chunks to variables

--- src/App.js
+++ src/App.js
@@ -11,28 +11,28 @@ class App extends React.Component {
     animeResults: [],
     yearsOptions: [],
     noResults: true,
-    searchOptions : {
-      year : new Date().getFullYear().toString(),
-      season : "summer",
+    searchOptions: {
+      year: new Date().getFullYear().toString(),
+      season: "summer",
     }
   }
 
-  onYearSeasonSearchHandler = ()=>{
+  onYearSeasonSearchHandler = () => {
     axios.get(`https://api.jikan.moe/v3/season/${this.state.searchOptions.year}/${this.state.searchOptions.season}`)
       .then(res => res.data)
       .then(res => {
-        this.setState({ animeResults: res.anime, noResults : false });
+        this.setState({ animeResults: res.anime, noResults: false });
       });
   }
 
-  onYearChange = (event)=>{
+  onYearChange = (event) => {
     let val = event.target.value;
-    this.setState({searchOptions : {year : val, season : this.state.searchOptions.season}});
+    this.setState({ searchOptions: { year: val, season: this.state.searchOptions.season } });
   }
 
-  onSeasonChange = (event)=>{
+  onSeasonChange = (event) => {
     let val = event.target.value;
-    this.setState({searchOptions : {year : this.state.searchOptions.year, season : val}});
+    this.setState({ searchOptions: { year: this.state.searchOptions.year, season: val } });
   }
 
   componentDidMount() {
@@ -41,56 +41,68 @@ class App extends React.Component {
     for (var i = currentYear; i >= 1950; --i) {
       years.push(i);
     }
-    this.setState({yearsOptions : years});
+    this.setState({ yearsOptions: years });
   }
 
   render() {
+    let header = (
+      <div className="header row">
+        AniANI-Jikan
+        </div>
+    );
+
+    let searchSeasonSection = (
+      <div className="row seasonSearchSection">
+        <div class="input-group col-md-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text">Year</span>
+          </div>
+          <select class="form-control" onChange={this.onYearChange}>
+            {
+              this.state.yearsOptions.map(q => <option>{q}</option>)
+            }
+          </select>
+        </div>
+        <div class="input-group col-md-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text">Season</span>
+          </div>
+          <select class="form-control" onChange={this.onSeasonChange}>
+            <option value="summer">summer</option>
+            <option value="winter">winter</option>
+            <option value="fall">fall</option>
+            <option value="spring">spring</option>
+          </select>
+        </div>
+        <div className="col-md-3">
+          <button className="btn btn-success" onClick={this.onYearSeasonSearchHandler}>Go</button>
+        </div>
+      </div>
+    );
+
+    let animeReasultsSection = (
+        <div className="anime-results row">
+          {
+            this.state.noResults ?
+              (<p>
+                NO anime results.
+            </p>)
+              :
+              this.state.animeResults.map(animeMini => <AnimeMini key={animeMini.mal_id} animeMini={animeMini} />)
+          }
+        </div>
+    );
+
     return (
       <div className="app-wrapper container">
-        <div className="header row">
-          AniANI-Jikan
-        </div>
+        {header}
         {/* <div className="searchSection row">
           <div className="col-md-6 offset-md-3">
             <input type="text" className="form-control" placeholder="Enter keyword to serach Anime" />
           </div>
         </div> */}
-        <div className="row seasonSearchSection">
-          <div class="input-group col-md-3">
-            <div class="input-group-prepend">
-              <span class="input-group-text">Year</span>
-            </div>
-            <select class="form-control" onChange={this.onYearChange}>
-              {
-                this.state.yearsOptions.map(q => <option>{q}</option>)
-              }
-            </select>
-          </div>
-          <div class="input-group col-md-3">
-            <div class="input-group-prepend">
-              <span class="input-group-text">Season</span>
-            </div>
-            <select class="form-control" onChange={this.onSeasonChange}>
-              <option value="summer">summer</option>
-              <option value="winter">winter</option>
-              <option value="fall">fall</option>
-              <option value="spring">spring</option>
-            </select>
-          </div>
-          <div className="col-md-3">
-            <button className="btn btn-success" onClick={this.onYearSeasonSearchHandler}>Go</button>
-          </div>
-        </div>
-        <div className="anime-results row">
-          {
-            this.state.noResults ?
-            (<p>
-              NO anime results.
-            </p>)
-            :
-            this.state.animeResults.map(animeMini => <AnimeMini key={animeMini.mal_id} animeMini={animeMini} />)
-          }
-        </div>
+        {searchSeasonSection}
+        {animeReasultsSection}
       </div>
     );
   }
