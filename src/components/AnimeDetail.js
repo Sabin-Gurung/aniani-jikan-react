cfcb12373eadbc8ff885a29ed0a58183899a33a4
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-10-16 15:41:19 +0200

    more details

--- src/components/AnimeDetail.js
+++ src/components/AnimeDetail.js
@@ -2,35 +2,45 @@ import React from "react";
 
 const axios = require("axios").default;
 
-class AnimeDetail extends React.Component{
-    state = {anime : null};
+class AnimeDetail extends React.Component {
+    state = { anime: null };
 
-    componentDidMount(){
+    componentDidMount() {
         axios.get(`https://api.jikan.moe/v3/anime/${this.props.animeId}`)
-        .then(res=>res.data)
-        .then(res=>{
-            this.setState({anime : res});
-        });
+            .then(res => res.data)
+            .then(res => {
+                this.setState({ anime: res });
+            });
     }
 
-    render(){
+    render() {
         let anime = this.state.anime;
-        return(
+        return (
             <>
-                <div className="col-md-12">
-                    {
-                        anime ? 
+                {
+                    anime ?
                         (
                             <>
-                            <h1>{anime.title}</h1>
-                            <img src={anime.image_url}></img>
-                            <button onClick={this.props.closeBtnHandler}>Close</button>
+                            <div className="col-md-12 row">
+                                <div className="col-md-4">
+                                    <img src={anime.image_url}></img>
+                                </div>
+                                <div className="col-md-8">
+                                    <button onClick={this.props.closeBtnHandler}>Close</button>
+                                    <h3>{anime.title}</h3>
+                                    <p>
+                                        {anime.synopsis}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="col-md-12">
+
+                            </div>
                             </>
-                        ) 
-                        : 
+                        )
+                        :
                         (<h1>Loading</h1>)
-                    }
-                </div>
+                }
             </>
         );
     }
