dd951a81369a86ede0024eba5d4b51096efc4d44
tejian <sabingurung68@gmail.com>
2020-09-12 11:30:29 +0200

    updated url

---
+++ docs/precache-manifest.4e1a90ade10d4f6692d87d5b522437d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754fa4fc759d3f76816a884c77dbf345",
+    "url": "/aniani-jikan-react/index.html"
+  },
+  {
+    "revision": "093f6eb8988fa137b71f",
+    "url": "/aniani-jikan-react/static/css/main.b98d418e.chunk.css"
+  },
+  {
+    "revision": "20f6e115cd6eb7911573",
+    "url": "/aniani-jikan-react/static/js/2.77161fe0.chunk.js"
+  },
+  {
+    "revision": "093f6eb8988fa137b71f",
+    "url": "/aniani-jikan-react/static/js/main.ca486f8d.chunk.js"
+  },
+  {
+    "revision": "13a0c7a4fc273b14d540",
+    "url": "/aniani-jikan-react/static/js/runtime-main.00f5de11.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7cb3ca65a6395394be9de04ead5995f7.js"
+  "/aniani-jikan-react/precache-manifest.4e1a90ade10d4f6692d87d5b522437d2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aniani-jikan-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
